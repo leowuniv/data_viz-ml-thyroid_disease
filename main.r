d58c4b427c5a29a4e96768f0d0809d8fca9990ce
Leo <leowuniv@gmail.com>
2025-11-20 14:28:44 -0800

    Update main.r

--- main.r
+++ main.r
@@ -3,3 +3,12 @@
 test <- c(1, 2, 3)
 
 print(test)
+
+# -----------------------
+
+# Command prompts to print data vizs
+
+while() {
+  print("==========================")
+  print("menu")
+  print("==========================")
