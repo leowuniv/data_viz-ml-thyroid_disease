1c9254e126e274b3a001ea01cb360c9d4559ccdc
rohankoditala <162520826+rohankoditala@users.noreply.github.com>
2025-11-23 20:37:01 -0800

    Imported some datasets
    Imported all possible datasets from the file with .data. I thought that there was supposed to be 8 total datafiles, but this one has nine for some reason.

--- data_cleaning.r
+++ data_cleaning.r
@@ -1,6 +1,17 @@
-# Load in files [attached to repository and Discord chat]
+#---
+title: "Data_Cleaning_Stats_20_Project"
+output: html_document
+---
 
-file1 <- read.csv("first_file.csv")
-head(file1)
-
-# ==================================
+#importing datasets
+```{r}
+allhypo <- read.csv("allhypo.data", header = TRUE) #import of allhypo.data file
+allbp <- read.csv("allhypo.data", header = TRUE)
+allrep <- read.csv("allrep.data", header = TRUE)
+dis <- read.csv("dis.data", header = TRUE)
+hypothyroid <- read.csv("hypothyroid.data", header = TRUE)
+new_thyroid <- read.csv("new-thyroid.data", header = TRUE) 
+sick_euthyroid <- read.csv("sick-euthyroid.data", header = TRUE)
+sick <- read.csv("sick.data", header = TRUE)
+thyroid0387 <- read.csv("thyroid0387.data", header = TRUE)
+```
