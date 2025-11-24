5c4f590ad86c599ee51ecbb940a2166dd0c72f86
Leo <leowuniv@gmail.com>
2025-11-23 23:16:25 -0800

    Update data_visualizations.r

--- data_visualizations.r
+++ data_visualizations.r
@@ -1,3 +1,12 @@
 # Research questions we plan to answer and analyze further:
 
+# =====================================
+
+#install.packages("ggplot2")
+#install.packages("tidyverse")
+
+library(ggplot2)
+
+# =====================================
+
 # Histogram
