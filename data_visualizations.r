bb423e7d47a805cba3bb3a4af6c51bc61c61bdcb
Leo <leowuniv@gmail.com>
2025-11-26 23:57:29 -0800

    Enhance comments and structure in data_visualizations.r
    Added comments for clarity and organized sections.

--- data_visualizations.r
+++ data_visualizations.r
@@ -5,8 +5,19 @@
 #install.packages("ggplot2")
 #install.packages("tidyverse")
 
+# =====================================
+
 library(ggplot2)
+
+# CSV files:
+# read.csv("") # void out later when merging into main file
 
 # =====================================
 
-# Histogram
+# Histogram 1 [Comparison of Age Distribution between Patients]
+
+ggplot(allbp, aes(x = Age)) + 
+  geom_histogram()
+
+# Plot 2
+
