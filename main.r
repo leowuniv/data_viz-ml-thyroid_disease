8edb03fa1c758016be983ca56a2581c8726007df
Leo <leowuniv@gmail.com>
2025-12-03 03:36:28 -0800

    Update main.r

--- main.r
+++ main.r
@@ -12,3 +12,9 @@ while() {
   print("==========================")
   print("menu")
   print("==========================")
+
+# -----------------------------------------
+
+# Make sure to replace the 0 in the file name with your group number from Google Sheets. --> Group 67
+# Make sure your Coding and Report files includes the names and UID numbers of all students in your group, but also every member of the group must submit the same collection of files for credit.
+# --> Located in the File Submission on BruinLearn
