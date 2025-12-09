631338f898b2cf89295b7c0ab4f9785e219947ba
Leo <leowuniv@gmail.com>
2025-12-08 22:31:40 -0800

    Update main.r

--- main.r
+++ main.r
@@ -1,20 +1,3 @@
 # Merge all branches into the main file of the project
 
-test <- c(1, 2, 3)
-
-print(test)
-
-# -----------------------
-
-# Command prompts to print data vizs
-n <- 1
-while(n > 0) {
-  print("==========================")
-  print("menu")
-  print("==========================")
-
-# -----------------------------------------
-
-# Make sure to replace the 0 in the file name with your group number from Google Sheets. --> Group 67
-# Make sure your Coding and Report files includes the names and UID numbers of all students in your group, but also every member of the group must submit the same collection of files for credit.
-# --> Located in the File Submission on BruinLearn
+# --> Compiled all into .Rmd file (Submitted)
