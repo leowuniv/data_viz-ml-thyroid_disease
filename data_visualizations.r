55070a6d60e08acc57080c750c914158f17b136a
Leo <leowuniv@gmail.com>
2025-12-08 23:06:09 -0800

    {R11} Future Goals - Clarify future implementation comment for menu visualization

--- data_visualizations.r
+++ data_visualizations.r
@@ -1196,6 +1196,6 @@ t.test(remove_nas_allhypo_age, mu = 50)
 # --------------------------------------------
 # {R11}
 
-# Future Implementation (Goals): Menu Visualization Integration
+# Future Implementation (Goal): Menu Visualization Integration
 
 # --------------------------------------------
