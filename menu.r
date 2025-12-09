034da486596bca61eb30f2d5a964a86cc0b359fb
Leo <leowuniv@gmail.com>
2025-12-08 22:31:59 -0800

    Update menu.r

--- menu.r
+++ menu.r
@@ -118,3 +118,25 @@ ggplot(allbp, aes(sex, fill=ThyroidClass)) +
 
 # =====================================
 
+# FROM main.r
+
+test <- c(1, 2, 3)
+
+print(test)
+
+# -----------------------
+
+# Command prompts to print data vizs
+n <- 1
+while(n > 0) {
+  print("==========================")
+  print("menu")
+  print("==========================")
+
+# -----------------------------------------
+
+# Make sure to replace the 0 in the file name with your group number from Google Sheets. --> Group 67
+# Make sure your Coding and Report files includes the names and UID numbers of all students in your group, but also every member of the group must submit the same collection of files for credit.
+# --> Located in the File Submission on BruinLearn
+
+
